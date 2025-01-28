1adbc0964dc9762d2ca3f355d8bcbbe88b2d08bf
Ugase <5bgdbdkuhskiy@gmail.com>
2025-01-29 00:39:06 +0200

    im fully insane

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
-use argon2::password_hash::rand_core::OsRng;
-use nspm::cryptography::{decrypt, encrypt, generate_salt, hash};
+use nspm::storage::PasswordArray;
 
-fn main() {}
+fn main() {
+    let dirname = String::from("haa");
+    let (service_dir, password_dir, salt_dir) = (
+        format!("{dirname}/serv"),
+        format!("{dirname}/pass"),
+        format!("{dirname}/salt"),
+    );
+    let master = String::from("here");
+    let mut pa = PasswordArray::new(master);
+    pa.add_password(String::from("here"), String::from("aa"));
+    dbg!(pa);
+}
